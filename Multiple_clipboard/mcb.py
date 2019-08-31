65605eb59d4d532b270ce572029fb89d8c45d5eb
Mingyi Bao <bmyjacks@outlook.com>
2019-08-31 18:25:09 +0800

    Update files

---
+++ Multiple_clipboard/mcb.py
@@ -0,0 +1,12 @@
+#! python3
+#HOStudio
+#LICENSE MIT USER LICENSE AGREEMENT
+
+import shelve, pyperclip, sys
+
+mcbShelf = shelve.open('mcb')
+
+#TODO:Save clipboard
+#TODO: List keywords and load content
+
+mcbShelf.close()
