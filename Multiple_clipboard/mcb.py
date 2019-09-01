cbe2ce90caf10be1d0485e3748338fab584d2287
Mingyi Bao <bmyjacks@outlook.com>
2019-09-01 15:30:32 +0800

    Update file

--- Multiple_clipboard/mcb.py
+++ Multiple_clipboard/mcb.py
@@ -13,7 +13,10 @@ import shelve, pyperclip, sys
 mcbShelf = shelve.open('mcb')
 
 #Save clipboard content.
-
+if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
+    mcbShelf[sys.argv[2] ] = pyperclip.paste()
+elif len (sys.argv) == 2:
+    
 
 #TODO: List keywords and load content
 
