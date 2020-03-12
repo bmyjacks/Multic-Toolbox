3af9f65e6e50dec8e9d184d163a61a8406cd37c9
bmyjacks <bmyjacks@outlook.com>
2020-03-12 20:38:36 +0800

    Update checkInput module

--- openzip/checkInput.py
+++ openzip/checkInput.py
@@ -1,19 +1,3 @@
-#! python3
-# -*- coding: utf-8 -*-
-# Copyright (c) 2020 bmyjacks
-
-import sys
-
-version = "0.0.0"
-
-file_name = "example.zip"
-
-
-print("This is openzip " + version)
-print("Welcome!")
-print("Input the file name:")
-file_name = str(input())
-print("File name is "+ file_name + " right?(Y or N)")
 correct = "Y"
 correct = input()
 if correct == "Y":
